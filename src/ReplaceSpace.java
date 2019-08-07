5e1c1809c30944a1c8bad92f796af070027e11d4
littleGnAl <tearjeaker@hotmail.com>
2019-08-08 00:01:22 +0800

    Replace space

--- src/ReplaceSpace.java
+++ src/ReplaceSpace.java
@@ -1,19 +1,30 @@
 /**
- * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+     * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
 public class ReplaceSpace {
 
     public String replaceSpace(StringBuffer str) {
         if (str.length() == 0) return str.toString();
-        int length = str.length() - 1;
-        int start = 0;
-        while (start <= length) {
-            int i = str.indexOf(" ", start);
-            if (i == -1) break;
-            str.replace(i, i + 1, "%20");
-            int preStart = start;
-            start += i;
-            if (preStart == start) break;
+
+        int spaceCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                ++spaceCount;
+            }
+        }
+
+        int originIndex = str.length() - 1;
+        int newLength = str.length() + 2 * spaceCount;
+        str.setLength(newLength);
+        int newIndex = newLength - 1;
+        for (; originIndex >= 0; originIndex--) {
+            if (str.charAt(originIndex) == ' ') {
+                str.setCharAt(newIndex--, '0');
+                str.setCharAt(newIndex--, '2');
+                str.setCharAt(newIndex--, '%');
+            } else {
+                str.setCharAt(newIndex--, str.charAt(originIndex));
+            }
         }
 
         return str.toString();
