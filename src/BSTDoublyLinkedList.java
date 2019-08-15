03ee05ac658989f62ebbde73db8e3376dff9b42a
littleGnAl <tearjeaker@hotmail.com>
2019-08-16 00:52:03 +0800

    WIP

--- src/BSTDoublyLinkedList.java
+++ src/BSTDoublyLinkedList.java
@@ -15,33 +15,55 @@ public class BSTDoublyLinkedList {
         }
     }
 
-    TreeNode convert(TreeNode root) {
-        if (root == null) return null;
-        convert(root.left);
-        convert(root.right);
-
-        if (root.left != null) {
-            root.left.right = root;
-        }
-
-        if (root.right != null) {
-            root.right.left = root;
-        }
-
-        return root.right;
-    }
-
-
+    private TreeNode lastNode = null;
 
     public TreeNode Convert(TreeNode pRootOfTree) {
-        TreeNode root = pRootOfTree;
-        TreeNode left = convert(root.left);
-        TreeNode right = convert(root.right);
-        left.right = root;
-        root.left = left;
-        right.left = root;
-        root.right = right;
+        if (pRootOfTree == null) return null;
+        if (pRootOfTree.left == null && pRootOfTree.right == null) {
+            lastNode = pRootOfTree;
+            return pRootOfTree;
+        }
 
-        return root;
+        TreeNode left = Convert(pRootOfTree.left);
+        if (left != null) {
+            lastNode.right = pRootOfTree;
+            pRootOfTree.left = lastNode;
+        }
+
+        lastNode = pRootOfTree;
+
+        TreeNode right = Convert(pRootOfTree.right);
+        if (right != null) {
+            right.left = pRootOfTree;
+            pRootOfTree.right = right;
+        }
+
+        return left == null ? pRootOfTree : left;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n10 = new TreeNode(10);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n12 = new TreeNode(12);
+        n10.left = n5;
+        n5.left = n3;
+        n5.right = n7;
+        n3.left = n1;
+        n3.right = n4;
+        n10.right = n12;
+
+        ArrayList<Integer> r = new ArrayList<>();
+        TreeNode n = new BSTDoublyLinkedList().Convert(n10);
+        System.out.println(n);
+        while (n != null) {
+            r.add(n.val);
+            n = n.right;
+        }
+
+        System.out.println(r.toString());
     }
 }
