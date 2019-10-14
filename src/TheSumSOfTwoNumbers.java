0ddbe07fe100545a3a68e2c50d04274fc73bd324
littleGnAl <tearjeaker@hotmail.com>
2019-10-14 15:40:01 +0800

    The sum S of two numbers

--- src/TheSumSOfTwoNumbers.java
+++ src/TheSumSOfTwoNumbers.java
@@ -8,33 +8,23 @@ import java.util.ArrayList;
 public class TheSumSOfTwoNumbers {
     public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
         ArrayList<Integer> result = new ArrayList<>();
+        if (array == null || array.length == 0) return result;
         int pLow = 0;
         int pHigh = array.length - 1;
-        int preNum1 = 0;
-        int preNum2 = 0;
 
         while (pLow < pHigh) {
             int num1 = array[pLow];
             int num2 = array[pHigh];
             int temp = num1 + num2;
             if (temp == sum) {
-                if (preNum1 * preNum2 == 0 || (preNum1 * preNum2 > num1 * num2)) {
-                    preNum1 = num1;
-                    preNum2 = num2;
-                }
-
-                ++pLow;
-                --pHigh;
+                result.add(num1);
+                result.add(num2);
+                return result;
             } else if (temp < sum) {
                 ++pLow;
             } else {
                 --pHigh;
             }
-        }
-
-        if (preNum1 != 0 && preNum2 != 0) {
-            result.add(preNum1);
-            result.add(preNum2);
         }
 
         return result;
